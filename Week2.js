d932627dd337140b1a89286d2f89c38a326643d3
caroolcanelas <carolcanelasg@gmail.com>
2022-01-07 16:20:45 -0300

    Fixed the forecast problem

--- Week2.js
+++ Week2.js
@@ -78,7 +78,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "8cbd64a63ba04c3afa29f0681a36cb68";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
